612a84c58e4acac4885dcb5f5df63c172cf9b225
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-03-06 15:31:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
 # code here!
+
+
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+
+
+def add_student
+
+  @roster["students_name"] = []
+  @roster["students_name"] << ["grade_level"]
+
+end
+
+
+end
